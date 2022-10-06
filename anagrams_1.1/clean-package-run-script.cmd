3b98059996244ecc21947dcb5d676cc3c311ac50
starkoracia <starkoracia@gmail.com>
2022-10-06 19:56:21 +0100

    feature(project) create project feature(anagrams package)
    Made first task "anagrams" with 3 classes:
     - StringReverser.java include general behavior;
     - AppTester.java with hardcoded test strings and test method;
     - AnagramsApp.java include main method.
    
    add clean-package-run-script.cmd to easy start.

---
+++ anagrams_1.1/clean-package-run-script.cmd
@@ -0,0 +1,4 @@
+call mvn clean package
+call clear
+call java -jar target/anagrams_1.1.jar
+call pause
---
+++ anagrams_1.1/src/main/java/com/foxminded/isaievdenys/anagrams/StringReverser.java
@@ -0,0 +1,43 @@
+package com.foxminded.isaievdenys.anagrams;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringReverser {
+
+    private StringReverser() {
+    }
+
+    public static String reverseWordsInStringExceptNonLetterSymbols(String sourceString) {
+        StringBuilder outputString = new StringBuilder();
+        String[] words = sourceString.split("\\s+");
+
+        Matcher spaceMatcher = Pattern.compile("\\s+").matcher(sourceString);
+        for (String word : words) {
+            outputString.append(reverseWordExceptNonLetterSymbols(word));
+            if (spaceMatcher.find()) {
+                outputString.append(spaceMatcher.group());
+            }
+        }
+        return outputString.toString();
+    }
+
+    private static String reverseWordExceptNonLetterSymbols(String sourceWord) {
+        StringBuilder outputWord = new StringBuilder(cutOutNonLetterSymbols(sourceWord));
+        outputWord.reverse();
+        insertNonLetterSymbols(outputWord, sourceWord);
+        return outputWord.toString();
+    }
+
+    private static void insertNonLetterSymbols(StringBuilder outputStringBuilder, String sourceWord) {
+        Matcher nonLetterSymbolsMatcher = Pattern.compile("[^a-zA-Z]+").matcher(sourceWord);
+        while (nonLetterSymbolsMatcher.find()) {
+            outputStringBuilder.insert(nonLetterSymbolsMatcher.start(), nonLetterSymbolsMatcher.group());
+        }
+    }
+
+    private static String cutOutNonLetterSymbols(String word) {
+        return String.join("", word.split("[^a-zA-Z]+"));
+    }
+
+}
